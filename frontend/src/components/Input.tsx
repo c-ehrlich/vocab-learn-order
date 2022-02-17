84b50c275883306d5e9a8333d414dfcb301ad91d
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-02-17 14:11:26 +0100

    feat(frontend): Parse user input & use in request

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -2,36 +2,52 @@ import { Fab, TextField } from '@mui/material';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import useStore from '../store';
 import { IServerResponse } from '../interfaces/IServerResponse';
+import { useState } from 'react';
 
 type Props = {};
 
 const Input = (props: Props) => {
+  const [textInput, setTextInput] = useState<string>('');
   const { setServerResponse } = useStore();
 
   const handleButtonClick = async () => {
-    const root = process.env.REACT_APP_SERVER;
-    const Response = await fetch(`${root}/api/learnorder`, {
-      method: 'POST',
-      // credentials: 'same-origin',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        words: ['食べる', '学校', 'レストラン', '舐達麻'],
-      }),
-    });
+    // parse input
+    let words: string[] = textInput
+      .replace(/(\(|（)(.[^()（）]*)(\)|）)/gm, ' ') // remove anything inside () or （）
+      .replace(/(\s|\n|,|、|・)+/gm, ' ') // remove delineation chars and consolidate whitespace
+      .split(' '); // turn into aray
+
+    // make request
+    const Response = await fetch(
+      `${process.env.REACT_APP_SERVER}/api/learnorder`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          words,
+        }),
+      }
+    );
     const data: IServerResponse = await Response.json();
-    setServerResponse(data);
+    if (data.words.length > 0) {
+      setServerResponse(data);
+    } else {
+      // TODO show an error
+      console.log('server sent back 0 results');
+    }
   };
 
   return (
     <>
       <TextField
         id='outlined-multiline-static'
-        label='Multiline'
+        label='Paste words here'
         multiline
         rows={4}
-        defaultValue='Default Value'
+        value={textInput}
+        onChange={(e) => setTextInput(e.target.value)}
       />
       <Fab color='primary' aria-label='add' onClick={handleButtonClick}>
         <KeyboardArrowRightIcon />
--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -3,12 +3,12 @@ import { IServerResponse } from './interfaces/IServerResponse';
 
 interface AppState {
   serverResponse: IServerResponse | null;
-  setServerResponse: (serverResponse: IServerResponse) => void;
+  setServerResponse: (serverResponse: IServerResponse | null) => void;
 }
 
 const useStore = create<AppState>((set) => ({
   serverResponse: null,
-  setServerResponse: (serverResponse: IServerResponse) => {
+  setServerResponse: (serverResponse: IServerResponse | null) => {
     set((state) => ({ ...state, serverResponse: serverResponse }));
   },
 }));
