c8c3bd4bc7ebc745a56fc22482741782bbf353c3
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-02-21 16:37:38 +0100

    feat(api): Ignore negative weightings

--- backend/src/utils/sortWords.ts
+++ backend/src/utils/sortWords.ts
@@ -14,15 +14,15 @@ function sortWords(words: IWord[], weights: IWordSortingWeights): IWord[] {
 
   function getWeightedWordRanking(word: IWord, weights: IWordSortingWeights): number {
     return (
-      (word.animeJDrama ? weights.animeJDrama / Math.sqrt(word.animeJDrama) : 0) +
-      (word.bccwj ? weights.bccwj / Math.sqrt(word.bccwj) : 0) +
-      (word.innocent ? weights.innocent / Math.sqrt(word.innocent) : 0) +
-      (word.kokugojiten ? weights.kokugojiten / Math.sqrt(word.kokugojiten) : 0) +
-      (word.narou ? weights.narou / Math.sqrt(word.narou) : 0) +
-      (word.netflix ? weights.netflix / Math.sqrt(word.netflix) : 0) +
-      (word.novels ? weights.novels / Math.sqrt(word.novels) : 0) +
-      (word.vn ? weights.vn / Math.sqrt(word.vn) : 0) +
-      (word.wikipedia ? weights.wikipedia / Math.sqrt(word.wikipedia) : 0)
+      (word.animeJDrama ? Math.max(weights.animeJDrama, 0) / Math.sqrt(word.animeJDrama) : 0) +
+      (word.bccwj ? Math.max(weights.bccwj, 0) / Math.sqrt(word.bccwj) : 0) +
+      (word.innocent ? Math.max(weights.innocent, 0) / Math.sqrt(word.innocent) : 0) +
+      (word.kokugojiten ? Math.max(weights.kokugojiten, 0) / Math.sqrt(word.kokugojiten) : 0) +
+      (word.narou ? Math.max(weights.narou, 0) / Math.sqrt(word.narou) : 0) +
+      (word.netflix ? Math.max(weights.netflix, 0) / Math.sqrt(word.netflix) : 0) +
+      (word.novels ? Math.max(weights.novels, 0) / Math.sqrt(word.novels) : 0) +
+      (word.vn ? Math.max(weights.vn, 0) / Math.sqrt(word.vn) : 0) +
+      (word.wikipedia ? Math.max(weights.wikipedia, 0) / Math.sqrt(word.wikipedia) : 0)
     );
   }
 }
