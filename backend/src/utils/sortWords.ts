219cda73a6f47bece6b3a6dd9b281d497d679c0c
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-06-29 20:49:12 +0200

    refactor: Simplify word sorting

--- backend/src/utils/sortWords.ts
+++ backend/src/utils/sortWords.ts
@@ -1,42 +1,41 @@
 import { TFrequencyListWeights, TWord } from '../schema/word.schema';
-import { TDuplicateList } from './findDuplicates';
 
 function sortWords(words: TWord[], weights: TFrequencyListWeights): TWord[] {
   return words.sort(compareWordForSorting);
 
+  // TODO refactor so compareWordForSorting doesn't have to live inside sortWords?
   function compareWordForSorting(a: TWord, b: TWord): number {
-    if (getWeightedWordRanking(a, weights) < getWeightedWordRanking(b, weights))
-      return 1;
-    return -1;
+    return getWeightedWordRanking(a, weights) <
+      getWeightedWordRanking(b, weights)
+      ? 1
+      : -1;
   }
+}
 
-  function getWeightedWordRanking(
-    word: TWord,
-    weights: TFrequencyListWeights
-  ): number {
-    const baseValue =
-      (word.animeJDrama
-        ? Math.max(weights.animeJDrama, 0) / Math.sqrt(word.animeJDrama)
-        : 0) +
-      (word.bccwj ? Math.max(weights.bccwj, 0) / Math.sqrt(word.bccwj) : 0) +
-      (word.innocent
-        ? Math.max(weights.innocent, 0) / Math.sqrt(word.innocent)
-        : 0) +
-      (word.kokugojiten
-        ? Math.max(weights.kokugojiten, 0) / Math.sqrt(word.kokugojiten)
-        : 0) +
-      (word.narou ? Math.max(weights.narou, 0) / Math.sqrt(word.narou) : 0) +
-      (word.netflix
-        ? Math.max(weights.netflix, 0) / Math.sqrt(word.netflix)
-        : 0) +
-      (word.novels ? Math.max(weights.novels, 0) / Math.sqrt(word.novels) : 0) +
-      (word.vn ? Math.max(weights.vn, 0) / Math.sqrt(word.vn) : 0) +
-      (word.wikipedia
-        ? Math.max(weights.wikipedia, 0) / Math.sqrt(word.wikipedia)
-        : 0);
+function getWeightedWordRanking(
+  word: TWord,
+  weights: TFrequencyListWeights
+): number {
+  const baseValue =
+    frequencyListValueCalc(word.animeJDrama, weights.animeJDrama) +
+    frequencyListValueCalc(word.bccwj, weights.bccwj) +
+    frequencyListValueCalc(word.innocent, weights.innocent) +
+    frequencyListValueCalc(word.kokugojiten, weights.kokugojiten) +
+    frequencyListValueCalc(word.narou, weights.narou) +
+    frequencyListValueCalc(word.netflix, weights.netflix) +
+    frequencyListValueCalc(word.novels, weights.novels) +
+    frequencyListValueCalc(word.vn, weights.vn) +
+    frequencyListValueCalc(word.wikipedia, weights.wikipedia);
 
-    return baseValue * (word.multiplier || 1);
-  }
+  return baseValue * (word.multiplier || 1);
+}
+
+function frequencyListValueCalc(
+  flValue: number | undefined,
+  flWeight: number
+): number {
+  if (!flValue) return 0;
+  return Math.max(flWeight, 0) / flValue;
 }
 
 export default sortWords;
